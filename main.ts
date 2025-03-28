b85416787d59a84f287a4c87980b1e926ec18387
benjamim12us <00001149604311sp@al.educacao.sp.gov.br>
2025-03-28 09:59:27 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+let x = 0
+input.onGesture(Gesture.Shake, function () {
+    x = randint(0, 2)
+    if (x == 1) {
+        basic.showIcon(IconNames.SmallDiamond)
+    } else if (x == 1) {
+    	
+    } else {
+    	
+    }
+})
